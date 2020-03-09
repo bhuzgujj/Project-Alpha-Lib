b964267a040e1b086150c8a3f1660fd691237709
Emile Boucher <1327897@cstjean.qc.ca>
2020-03-09 08:52:51 -0400

    0.1.2

--- PersonnalLib/CombatSimulation.cs
+++ PersonnalLib/CombatSimulation.cs
@@ -4,7 +4,8 @@ namespace PersonnalLib
 {
     class CombatSimulation
     {
+        public const double MidigationBase = 10;
         public static int SimpleDamageCalculation(double p_offensiveStats, double p_poucentageMidigated) => (int)(p_offensiveStats * p_poucentageMidigated);
-
+        public static double MidigationCalculation(double p_defense) => 1 - (MidigationBase / MidigationBase * p_defense);
     }
 }
